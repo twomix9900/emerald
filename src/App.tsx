888670b08f8483ddca74e5a2577ca5b49a95b86b
Kay <ds.kay.kim@gmail.com>
2022-07-05 09:17:54 -0700

    init router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello Emerald
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Hello Emerald!</h2>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
